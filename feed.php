67713ce7ddda1a0bd1dc20790c88aa4e8d36a958
haneki <hanekihyouka@gmail.com>
2023-10-04 09:45:01 +0800

    imma 摆烂

--- feed.php
+++ feed.php
@@ -1,5 +1,5 @@
 <?php
-$feed_url = "https://your_blog/feed";
+$feed_url = "https://haneki.100oj.com/index.xml";
 $data = simplexml_load_file($feed_url)->channel->item;
 $resp = [
     "status" => 0,
